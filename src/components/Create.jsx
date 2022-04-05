2173313c707aefb92448a52d87a5418466198cf3
pulgueta <retardix456@gmail.com>
2022-04-05 01:54:41 -0500

    toast

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { collection, addDoc } from "firebase/firestore";
+
+import { db } from "../../firebase.config";
 
 const Create = () => {
-  const createPost = async (e) => {
+  const [data, setData] = useState({
+    title: "",
+    author: "",
+    body: "",
+  });
+
+  const titleChange = (ev) => {
+    setData({ ...data, title: ev.target.value });
+  };
+
+  const authorChange = (ev) => {
+    setData({ ...data, author: ev.target.value });
+  };
+
+  const bodyChange = (ev) => {
+    setData({ ...data, body: ev.target.value });
+  };
+
+  const collectionRef = collection(db, "posts");
+
+  const createPost = (e) => {
     e.preventDefault();
+    addDoc(collectionRef, {
+      title: data.title,
+      author: data.author,
+      descBody: data.body,
+      createdAt: new Date(),
+    })
+      .then(() => {
+        toast.success("Post created!");
+      })
+      .catch(() => {
+        toast.error("Whoops! Something went wrong!");
+      });
+
+    setData({
+      title: "",
+      author: "",
+      body: "",
+    });
   };
 
   return (
     <div className="wrapper create">
+      <Toaster />
       <form>
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Author" />
-        <textarea placeholder="Description" />
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={titleChange}
+          value={data.title}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          onChange={authorChange}
+          value={data.author}
+        />
+        <textarea
+          placeholder="Description"
+          onChange={bodyChange}
+          value={data.body}
+        />
         <button type="submit" onClick={createPost}>
           Submit
         </button>
