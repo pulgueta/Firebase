a1fade248cd922c40dee1f289638588e7fc09197
pulgueta <retardix456@gmail.com>
2022-04-05 10:18:26 -0500

    toast skeleton

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,34 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
-import { collection, getDocs } from "firebase/firestore";
+import toast, { Toaster } from "react-hot-toast";
+import { doc, collection, getDocs, deleteDoc } from "firebase/firestore";
 
 import { db } from "../../firebase.config";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const Posts = () => {
   const [value, setValue] = useState([]);
+  const [loading, setLoading] = useState(false);
   const postCollection = collection(db, "posts");
 
   useEffect(() => {
     const getUser = async () => {
+      setLoading(true);
       const data = await getDocs(postCollection);
-      setValue(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-
-      console.log(setValue);
+      setValue(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+        setLoading(false)
+      );
     };
 
     getUser();
   }, []);
+
+  const deletePost = async (id) => {
+    await deleteDoc(doc(db, "posts", id))
+      .then(toast.success("Post deleted!"))
+      .catch(toast.error("Whoops! Something went wrong!"));
+  };
   return (
     <div className="wrapper posts">
+      <Toaster />
       {value.map((user) => {
         return (
           <div key={user.id}>
             <div className="post">
-              <h1 className="header">{user.title || <Skeleton />}</h1>
+              <h1 className="header">{loading ? <Skeleton /> : user.title}</h1>
               <hr />
-              <h3 className="author">{user.author || <Skeleton />}</h3>
-              <p className="desc">{user.descBody || <Skeleton />}</p>
+              <h3 className="author">{loading ? <Skeleton /> : user.author}</h3>
+              <p className="desc">{loading ? <Skeleton /> : user.descBody}</p>
+              <button className="del-btn" onClick={() => deletePost(user.id)}>
+                Delete
+              </button>
             </div>
           </div>
         );
